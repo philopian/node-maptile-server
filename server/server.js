68fd5701022786795604938ff3a03000b7c1b96c
philopian <pdotwdot@gmail.com>
2015-02-05 18:16:38 -0800

    added a get REST endpoint to get the tiles

--- server/server.js
+++ server/server.js
@@ -1,12 +1,36 @@
 var path    = require('path');
+var MBTiles = require('mbtiles');
 var express = require('express');
+
 
 var app = express();
 var clientPath = path.resolve(__dirname, '../webClient');
-
+var tilesDir = path.resolve(__dirname, '../tiles');
 
 
 app.use(express.static(clientPath));
+
+
+app.get("/:s/:z/:x/:y.*", function(req,res){
+
+    new MBTiles(path.join(tilesDir, req.params.s+'.mbtiles'),function(err, mbtiles){
+        mbtiles.getTile(req.params.z, req.params.x, req.params.y ,function(err,tile,headers){
+
+            if(err) {
+                res.send('...Tile rendering error: '+err+"\n");
+            } else {
+                res.header("Content-Type", "image/png");
+                res.send(tile);
+            }
+
+        });//getTiles
+        if(err) console.log(".......error opening sqlite database");
+    });//MBTiles
+
+});//app.get()
+
+
+
 
 
 //--Make the app listen on a port-------------
