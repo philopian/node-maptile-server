b70b1b9481421b1fb626e58f832609c53bbf8c16
philopian <pdotwdot@gmail.com>
2015-02-05 18:29:37 -0800

    frontend: added leaflet code and point to the tiles from the api

--- webClient/app/mapView/mapView.controller.js
+++ webClient/app/mapView/mapView.controller.js
@@ -3,8 +3,13 @@
 angular.module('customTilesApp')
 .controller('MapViewCtrl', function ($scope) {
 
-	$scope.awesomeThings = [
-		"yeoman", "angularjs", "nodejs", "express", "mongodb", "jwt", "gulp", "passport", "spinjs", "animate.css"
-	];
+    /* create leaflet map */
+    var map = L.map('map', {
+        center: [39.5, -98.35],
+        zoom: 4,
+        minZoom: 0,
+        maxZoom: 8
+    });
+    new L.tileLayer('http://localhost:5000/road-trip/{z}/{x}/{y}.png', {}).addTo(map);
 
 });
