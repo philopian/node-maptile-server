34df1f47800981b4a0189159d12c963e38bcd4e4
C Foster <cfoster@hdrinc.com>
2015-02-06 12:06:10 -0800

    Switch port; Haver server.js host the home page

--- server/server.js
+++ server/server.js
@@ -1,40 +1,40 @@
-var path    = require('path');
+var path = require('path');
 var MBTiles = require('mbtiles');
 var express = require('express');
 
 
 var app = express();
-var clientPath = path.resolve(__dirname, '../webClient');
+//var clientPath = path.resolve(__dirname, '../webClient');
+var clientPath = path.resolve("..") + "/webClient/";
 var tilesDir = path.resolve(__dirname, '../tiles');
 
 
 app.use(express.static(clientPath));
 
+app.get("/:s/:z/:x/:y.*", function(req, res) {
 
-app.get("/:s/:z/:x/:y.*", function(req,res){
+	new MBTiles(path.join(tilesDir, req.params.s + '.mbtiles'), function(err,
+		mbtiles) {
+		mbtiles.getTile(req.params.z, req.params.x, req.params.y, function(err,
+			tile, headers) {
 
-    new MBTiles(path.join(tilesDir, req.params.s+'.mbtiles'),function(err, mbtiles){
-        mbtiles.getTile(req.params.z, req.params.x, req.params.y ,function(err,tile,headers){
+			if (err) {
+				res.send('...Tile rendering error: ' + err + "\n");
+			} else {
+				res.header("Content-Type", "image/png");
+				res.send(tile);
+			}
 
-            if(err) {
-                res.send('...Tile rendering error: '+err+"\n");
-            } else {
-                res.header("Content-Type", "image/png");
-                res.send(tile);
-            }
+		}); //getTiles
+		if (err) console.log(".......error opening sqlite database");
+	}); //MBTiles
 
-        });//getTiles
-        if(err) console.log(".......error opening sqlite database");
-    });//MBTiles
-
-});//app.get()
-
-
+}); //app.get()
 
 
 
 //--Make the app listen on a port-------------
-var port = process.env.PORT || 5000;
-app.listen(port, function(){
-    console.log('The magic happens on port: '+port);
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+	console.log('The magic happens on port: ' + port);
 });
--- webClient/app/mapView/mapView.controller.js
+++ webClient/app/mapView/mapView.controller.js
@@ -1,15 +1,16 @@
 'use strict';
 
 angular.module('customTilesApp')
-.controller('MapViewCtrl', function ($scope) {
+	.controller('MapViewCtrl', function($scope) {
 
-    /* create leaflet map */
-    var map = L.map('map', {
-        center: [39.5, -98.35],
-        zoom: 4,
-        minZoom: 0,
-        maxZoom: 8
-    });
-    new L.tileLayer('http://localhost:5000/road-trip/{z}/{x}/{y}.png', {}).addTo(map);
+		/* create leaflet map */
+		var map = L.map('map', {
+			center: [39.5, -98.35],
+			zoom: 4,
+			minZoom: 0,
+			maxZoom: 8
+		});
+		new L.tileLayer('http://localhost:8080/road-trip/{z}/{x}/{y}.png', {}).addTo(
+			map);
 
-});
+	});
